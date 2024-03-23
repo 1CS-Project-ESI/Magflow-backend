bf4b0c3641a9e5efc7bca23bdc85c9de07a65049
Waelalg <mo.lebaili@esi-sba.dz>
2024-03-23 03:49:32 +0100

    is admin function is done

--- middlewares/validateTokenHandler.js
+++ middlewares/validateTokenHandler.js
@@ -1,27 +1,31 @@
 import asyncHandler from "express-async-handler";
 import jwt from "jsonwebtoken";
+import { User } from "../models/usersModel.js";
 
-const validateToken= asyncHandler(async (req,res,next) => {
-    // variable
-    let token;
-    let authHeader = req.headers.authorization;
-    if (authHeader && authHeader.startsWith("Bearer")){
-    // psq token yjiw f debut gae Bearer then space 
-    token = authHeader.split(" ")[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
-        if (err){
-            res.status(401).json({message : 'User is not authorized'});
+const validateToken = asyncHandler(async (req, res, next) => {
+    try {
+        let token;
+        let authHeader = req.headers.authorization;
+        if (authHeader && authHeader.startsWith("Bearer")) {
+            token = authHeader.split(" ")[1];
+            const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+            const userId = decoded.id; // Assuming the user ID is stored in the token
+
+            // Fetch user from the database using the user ID
+            const user = await User.findByPk(userId);
+            if (!user) {
+                return res.status(401).json({ message: 'User not found' });
+            }
+
+            // Attach the user object to the request
+            req.user = user;
+            next();
+        } else {
+            return res.status(401).json({ message: 'User is not authorized or token expired' });
         }
-        // i've verified the token and i have extracted the information which embedded in the token
-        req.user = decoded.user;
-        next();
-
-    })
-    if (!token){
-        res.status(401).json({message : 'User is not authorized or token expired'});
+    } catch (error) {
+        return res.status(401).json({ message: 'User is not authorized or token expired' });
     }
-}
-
 });
 
-export {validateToken};
+export { validateToken };

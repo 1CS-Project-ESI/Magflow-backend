5d8a7b849c8403fefb1bef414ee4955292f17970
Th3-l4dy <f.bencharef@esi-sba.dz>
2024-03-27 15:04:33 +0100

    routes/userRoutes.js modifications

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -9,20 +9,23 @@ import   {createBackup , getAllBackups}  from '../controllers/backupControllers.
 
 const router = express.Router();
 
+
+import cors from "cors";
+router.use(cors());
 // Define routes (endpoints)
 
-router.delete("/:id",validateToken ,deleteUserById);
-router.get("/AllUsers", validateToken,getAllUsers);
-router.put("/modifyParams",validateToken,modifyParams);
-router.put("/:id",validateToken,updateUserById);
-router.post("/createuser", validateToken, isAdmin,createUser);
+router.delete("/:id",validateToken , isAdmin ,deleteUserById);
+router.get("/AllUsers", validateToken, isAdmin ,getAllUsers);
+router.put("/modifyParams",validateToken, isAdmin ,modifyParams);
+router.put("/:id",validateToken, isAdmin ,updateUserById);
+router.post("/createuser", validateToken, isAdmin ,createUser); 
 router.post("/deactivateaccount" , validateToken , isAdmin , deactivateAccount );
 router.post("/activateaccount" , validateToken , isAdmin , activateAccount );
-router.get("/currentuser",validateToken,currentUser)
+router.get("/currentuser",validateToken, isAdmin ,currentUser)
 
-router.post('/createbackups', createBackup);
-router.get("/getAllBackups",  getAllBackups)
-// router.get all backups 
+router.post('/createbackups', validateToken , isAdmin, createBackup);
+router.get("/getAllBackups", validateToken , isAdmin, getAllBackups)
+
 
 
 export default router;

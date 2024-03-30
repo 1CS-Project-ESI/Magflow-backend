ce6faf01908d56f5a9b395a46eb85502fd82f05e
Waelalg <mo.lebaili@esi-sba.dz>
2024-03-30 14:07:15 +0100

    get all roles added

--- routes/rolesRoutes.js
+++ routes/rolesRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
 import {validateToken} from "../middlewares/validateTokenHandler.js";
-import { updateRolePermissions } from "../controllers/rolesController.js"
+import { updateRolePermissions , getAllRoles} from "../controllers/rolesController.js"
 
 const router = express.Router();
 
 // router.post('/add', validateToken, addRole);
 router.post('/update/:id', validateToken, updateRolePermissions);
-
+router.get('/allroles',getAllRoles)
 export default router;

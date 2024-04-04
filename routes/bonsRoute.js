689ff30c98daf5e2450ec398da5b73d663f92b22
houyembae <lh.bouchelarem@esi-sba.dz>
2024-04-04 03:30:23 +0100

    bon de réception done

--- routes/bonsRoute.js
+++ routes/bonsRoute.js
@@ -1,12 +1,13 @@
 import express from "express";
 import {validateToken} from "../middlewares/validateTokenHandler.js";
 
-import { createBonCommande } from "../controllers/bonsController.js";
+import { createBonCommande, createBonRepection } from "../controllers/bonsController.js";
 
 
 const router = express.Router();
 
 
-router.post('/create/:id_agentServiceAchat',createBonCommande);
+router.post('/create-bon-commande/:id_agentServiceAchat',validateToken, createBonCommande);
+router.post('/create-bon-reception/:id_magasinier', createBonRepection);
 
 export default router;

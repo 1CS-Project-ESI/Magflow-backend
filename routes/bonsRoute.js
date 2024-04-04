b2cfb8a0d2177b71df8bb651a0841a81ab85174b
Waelalg <mo.lebaili@esi-sba.dz>
2024-04-04 01:40:36 +0100

    get all commands and get products of a command done

--- routes/bonsRoute.js
+++ routes/bonsRoute.js
@@ -1,12 +1,14 @@
 import express from "express";
 import {validateToken} from "../middlewares/validateTokenHandler.js";
 
-import { createBonCommande } from "../controllers/bonsController.js";
+import { createBonCommande ,getAllCommands ,getAllProductsOfCommand} from "../controllers/bonsController.js";
 
 
 const router = express.Router();
 
 
-router.post('/create/:id_agentServiceAchat',createBonCommande);
+router.post('/create/:id_agentServiceAchat',validateToken,createBonCommande);
+router.get('/allcommands',getAllCommands);
+router.get('/commandproducts/:command_id',getAllProductsOfCommand)
 
 export default router;

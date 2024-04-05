58bea6d717a73f070a78bc353180d6008e7bd80b
houyembae <lh.bouchelarem@esi-sba.dz>
2024-04-05 04:19:39 +0100

    fournisseur CRUD done

---
+++ controllers/fournisseurController.js
@@ -0,0 +1,87 @@
+import { Fournisseur } from '../models/fournisseurModel.js'; 
+
+
+const createFournisseur = async (req, res) => {
+    try {
+
+        const { name, email, phone, rc, nif, rib } = req.body;
+        
+        const existingFournisseur = await Fournisseur.findOne({ where: { email } });
+        if (existingFournisseur) {
+            return res.status(400).json({ message: 'Email is already in use' });
+        }
+
+        const fournisseur = await Fournisseur.create({
+            name,
+            email,
+            phone,
+            rc,
+            nif,
+            rib
+        });
+
+        res.status(201).json({ message: 'Supplier created successfully', fournisseur });
+    } catch (error) {
+
+        console.error('Failed to create supplier:', error);
+        res.status(500).json({ message: 'Failed to create supplier', error: error.message });
+    }
+};
+
+
+const getAllFournisseurs = async (req, res) => {
+    try {
+        const fournisseurs = await Fournisseur.findAll();
+        res.status(200).json({ fournisseurs });
+    } catch (error) {
+        res.status(500).json({ message: 'Failed to fetch suppliers', error: error.message });
+    }
+};
+
+
+
+const updateFournisseurByEmail = async (req, res) => {
+    try {
+      const { email } = req.params;
+      const [updated] = await Fournisseur.update(req.body, {
+        where: { email: email },
+      });
+      if (updated) {
+        const updatedFournisseur = await Fournisseur.findOne({
+          where: { email: email },
+        });
+        res.status(200).json({
+          message: 'Supplier updated successfully',
+          fournisseur: updatedFournisseur,
+        });
+      } else {
+        res.status(404).json({ message: 'Supplier not found' });
+      }
+    } catch (error) {
+      res.status(500).json({
+        message: 'Failed to update supplier',
+        error: error.message,
+      });
+    }
+  };
+
+
+  const deleteFournisseurByEmail = async (req, res) => {
+    try {
+      const { email } = req.params;
+      const deleted = await Fournisseur.destroy({ where: { email: email } });
+      if (deleted) {
+        res.status(200).json({ message: 'Supplier deleted successfully' });
+      } else {
+        res.status(404).json({ message: 'Supplier not found' });
+      }
+    } catch (error) {
+      res.status(500).json({
+        message: 'Failed to delete supplier',
+        error: error.message,
+      });
+    }
+  };
+
+
+export { createFournisseur, getAllFournisseurs, updateFournisseurByEmail, deleteFournisseurByEmail };

0551788c0c30f4c191699248a0e9a531f663644f
houyembae <lh.bouchelarem@esi-sba.dz>
2024-04-05 06:52:46 +0100

    get all receptions done

--- routes/bonsRoute.js
+++ routes/bonsRoute.js
@@ -1,7 +1,7 @@
 import express from "express";
 import {validateToken} from "../middlewares/validateTokenHandler.js";
 
-import { createBonCommande ,createBonRepection, getAllCommands ,getAllProductsOfCommand, RemainingProducts} from "../controllers/bonsController.js";
+import { createBonCommande ,createBonRepection, getAllCommands, getAllReception,getAllProductsOfCommand, RemainingProducts} from "../controllers/bonsController.js";
 
 const router = express.Router();
 
@@ -9,6 +9,7 @@ const router = express.Router();
 router.post('/create/:id_agentServiceAchat',validateToken,createBonCommande);
 router.post('/create-bon-reception/:id_magasinier',validateToken, createBonRepection);
 router.get('/allcommands',validateToken, getAllCommands);
+router.get('/allreceptions',validateToken,getAllReception);
 router.get('/commandproducts/:command_id',validateToken,getAllProductsOfCommand);
 router.get('/remaining-products/:ReceptionId',validateToken, RemainingProducts)
 

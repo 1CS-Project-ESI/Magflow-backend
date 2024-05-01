53757514b3697d21c1be4db43afa143e7a060dda
Waelalg <mo.lebaili@esi-sba.dz>
2024-05-01 22:24:47 +0100

    command externe

--- routes/pdfRoutes.js
+++ routes/pdfRoutes.js
@@ -2,11 +2,12 @@ import express from "express";
 
 const router = express.Router();
 import cors from "cors";
-import {generatePDF,generateBonSortiePDF} from "../controllers/pdfBonCommandeController.js";
+import {generatePDF,generateBonSortiePDF, generateBonCommandePDF} from "../controllers/pdfBonCommandeController.js";
 router.use(cors());
 
 router.get('/pdfcommandeinterne/:bonCommandeInterneId' , generatePDF)
 router.get('/pdfbonsortie/:bonSortieId' , generateBonSortiePDF)
+router.get('/pdfboncommande/:bonCommandeId',generateBonCommandePDF)
 
 
 export default router;

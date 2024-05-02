23a149574fe176efbdd54b2c21f5de45db5ac599
Waelalg <mo.lebaili@esi-sba.dz>
2024-05-02 01:03:20 +0100

    bon reception pdf

--- routes/pdfRoutes.js
+++ routes/pdfRoutes.js
@@ -2,12 +2,13 @@ import express from "express";
 
 const router = express.Router();
 import cors from "cors";
-import {generatePDF,generateBonSortiePDF, generateBonCommandePDF} from "../controllers/pdfBonCommandeController.js";
+import {generatePDF,generateBonSortiePDF, generateBonCommandePDF, generateBonReceptionPDF} from "../controllers/pdfBonCommandeController.js";
 router.use(cors());
 
 router.get('/pdfcommandeinterne/:bonCommandeInterneId' , generatePDF)
 router.get('/pdfbonsortie/:bonSortieId' , generateBonSortiePDF)
 router.get('/pdfboncommande/:bonCommandeId',generateBonCommandePDF)
+router.get('/pdfbonreception/:bonReceptionId',generateBonReceptionPDF)
 
 
 export default router;

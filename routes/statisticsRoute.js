da2f1b6ef0bdecbf4d6b31790085239f96005319
Waelalg <mo.lebaili@esi-sba.dz>
2024-05-19 18:32:19 +0100

    sssssss

--- routes/statisticsRoute.js
+++ routes/statisticsRoute.js
@@ -1,12 +1,14 @@
 import express from "express";
-import {calculateQuantitiesByProduct , calculateStockValue, getMostConsumableProductsByStructure,fetchProductsWithPositiveStock, getMostConsumableProductsByUser} from "../controllers/statisticsControllers.js"
+import {calculateQuantitiesByProduct , calculateStockValue, getMostConsumableProductsByStructure,fetchProductsWithPositiveStock, getMostConsumableProductsByUser , getTopConsumersByStructure , getTotalOrdersByStructure} from "../controllers/statisticsControllers.js"
 
 const router = express.Router();
 
-router.get('/mostconsumableproducts', calculateQuantitiesByProduct)
-router.get('/mostconsumableproductsbystructure/:structureId',getMostConsumableProductsByStructure)
-router.get('/stockvalue',calculateStockValue)
-router.get('/remaining',fetchProductsWithPositiveStock)
-router.get('/mostconsumableproductsbyuser/:user_id',getMostConsumableProductsByUser)
+router.get('/mostconsumableproducts', calculateQuantitiesByProduct);
+router.get('/mostconsumableproductsbystructure/:structureId',getMostConsumableProductsByStructure);
+router.get('/stockvalue',calculateStockValue);
+router.get('/remaining',fetchProductsWithPositiveStock);
+router.get('/mostconsumableproductsbyuser/:user_id',getMostConsumableProductsByUser);
+router.get('/gettopconsumersbystructure/:structureId',getTopConsumersByStructure);
+router.get('/getTotalOrdersByStructure/:structureId' , getTotalOrdersByStructure)
 
 export default router;

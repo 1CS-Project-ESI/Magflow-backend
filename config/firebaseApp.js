52da91ee05b0a60242212fa01e954d9c17a5814f
Waelalg <mo.lebaili@esi-sba.dz>
2024-06-05 16:59:13 +0100

    stat1

--- config/firebaseApp.js
+++ config/firebaseApp.js
@@ -2,7 +2,6 @@ import fs from 'fs';
 import admin from 'firebase-admin'
 
 const jsonData = JSON.parse(fs.readFileSync('./config/pushNotification.json', 'utf-8'));
-console.log(jsonData);
 admin.initializeApp({
   credential: admin.credential.cert(jsonData)
 });
